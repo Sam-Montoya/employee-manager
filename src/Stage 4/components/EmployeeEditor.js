81292b987e93c0fb62123dc402785b3a29be3fed
Samuel <samuel.montoya1073@gmail.com>
2017-08-14 14:24:23 -0600

    Finished Stage 4.

--- src/Stage 4/components/EmployeeEditor.js
+++ src/Stage 4/components/EmployeeEditor.js
@@ -11,8 +11,23 @@ class EmployeeEditor extends Component {
   }
 
   // componentWillReceiveProps
-
+  componentWillReceiveProps(props){
+    this.setState({
+      employee: Object.assign({}, props.selected),
+      originalEmployee: props.selected,
+      notModified: true
+    });
+  }
   // handleChange
+  handleChange(prop, val){
+    if(this.state.notModified === true){
+      this.setState({ notModified: false});
+    }
+
+    var employeeCopy = Object.assign({}, this.state.employee);
+    employeeCopy[prop] = val;
+    this.setState({employee: employeeCopy});
+  }
 
   save() {
     this.state.originalEmployee.updateName(this.state.employee.name);
